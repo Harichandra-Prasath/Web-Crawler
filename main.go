67652f0f8abbdc1773597441b341b7c8a319f6ab
Harichandra-Prasath <1hcp302004@gmail.com>
2024-01-06 12:01:58 +0530

    moved to utils

--- main.go
+++ main.go
@@ -3,42 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
+	"github.com/Harichandra-Prasath/Web-Crawler/utils"
 	"golang.org/x/net/html"
 )
-
-func Getallnodes(node *html.Node, q *Queue) {
-	var crawl func(*html.Node)
-	crawl = func(node *html.Node) {
-		if node.Type == html.ElementNode && node.Data == "a" {
-			for _, attr := range node.Attr {
-				if attr.Key == "href" {
-					_, ok := q.history[attr.Val]
-					if strings.HasPrefix(attr.Val, "https://scrapeme.live/shop/") && !ok {
-						q.append(attr.Val)
-					}
-
-				}
-			}
-		}
-		for child := node.FirstChild; child != nil; child = child.NextSibling {
-			crawl(child)
-		}
-	}
-
-	crawl(node)
-}
 
 var (
 	failed int
 )
 
-func worker(q *Queue, wg *sync.WaitGroup) {
+func worker(q *utils.Queue, wg *sync.WaitGroup) {
 	defer wg.Done()
-	curr := q.popleft()
+	curr := q.Popleft()
 	if curr == "" {
 		return
 	}
@@ -48,13 +26,12 @@ func worker(q *Queue, wg *sync.WaitGroup) {
 		return
 	}
 	doc, _ := html.Parse(res.Body)
-	Getallnodes(doc, q)
+	utils.Getallnodes(doc, q)
 }
 
 func main() {
-	q := &Queue{}
-	q.history = make(map[string]bool)
-	q.append("https://scrapeme.live/shop/")
+	q := utils.GetQueue()
+	q.Append("https://scrapeme.live/shop/")
 	wg := new(sync.WaitGroup)
 	start := time.Now()
 	for len(q.Elements) > 0 {
@@ -65,33 +42,8 @@ func main() {
 		wg.Wait()
 	}
 	fmt.Println(len(q.Elements))
-	fmt.Println(len(q.history))
+	fmt.Println(len(q.History))
 	fmt.Println(failed)
 	fmt.Println(time.Since(start))
-
-}
-
-type Queue struct {
-	Elements []string
-	history  map[string]bool
-}
-
-func (q *Queue) append(element string) {
-	q.Elements = append(q.Elements, element)
-	q.history[element] = true
-}
-
-func (q *Queue) popleft() string {
-	if len(q.Elements) > 0 {
-		element := q.Elements[0]
-		if len(q.Elements) == 1 {
-			q.Elements = nil
-			return element
-		}
-		q.Elements = q.Elements[1:]
-		return element
-	} else {
-		return ""
-	}
 
 }
---
+++ utils/utils.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Queue struct {
+	Elements []string
+	History  map[string]bool
+}
+
+func (q *Queue) Append(element string) {
+	q.Elements = append(q.Elements, element)
+	q.History[element] = true
+}
+
+func (q *Queue) Popleft() string {
+	if len(q.Elements) == 0 {
+		return ""
+	} else {
+		element := q.Elements[0]
+		q.Elements = q.Elements[1:]
+		return element
+	}
+
+}
+
+func GetQueue() *Queue {
+	q := &Queue{}
+	q.History = make(map[string]bool)
+	return q
+}
+
+func Getallnodes(node *html.Node, q *Queue) {
+	var crawl func(*html.Node)
+	crawl = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, attr := range node.Attr {
+				if attr.Key == "href" {
+					_, ok := q.History[attr.Val]
+					if strings.HasPrefix(attr.Val, "https://scrapeme.live/shop/") && !ok {
+						q.Append(attr.Val)
+					}
+
+				}
+			}
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			crawl(child)
+		}
+	}
+
+	crawl(node)
+}
